5815f789f690a74190c649237e12d7fa0fea4715
kholida magfirah <kholidamagfirah161001@gmail.com>
2021-12-10 20:15:35 +0700

    PRAKTIKUM 6

---
+++ P6_1_1204003/P6_1_1204003/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace P6_1_1204003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtIdProdi = new System.Windows.Forms.TextBox();
+            this.txtNamaProdi = new System.Windows.Forms.TextBox();
+            this.txtSingkatan = new System.Windows.Forms.TextBox();
+            this.txtKaProdi = new System.Windows.Forms.TextBox();
+            this.txtSekProdi = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FORM INPUT PRODI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID Prodi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nama Prodi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Singkatan";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ketua Prodi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(44, 330);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Sekretaris Prodi ";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(81, 383);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "&Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(285, 383);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtIdProdi
+            // 
+            this.txtIdProdi.Location = new System.Drawing.Point(239, 88);
+            this.txtIdProdi.Name = "txtIdProdi";
+            this.txtIdProdi.Size = new System.Drawing.Size(100, 22);
+            this.txtIdProdi.TabIndex = 8;
+            // 
+            // txtNamaProdi
+            // 
+            this.txtNamaProdi.Location = new System.Drawing.Point(239, 144);
+            this.txtNamaProdi.Name = "txtNamaProdi";
+            this.txtNamaProdi.Size = new System.Drawing.Size(100, 22);
+            this.txtNamaProdi.TabIndex = 9;
+            // 
+            // txtSingkatan
+            // 
+            this.txtSingkatan.Location = new System.Drawing.Point(239, 200);
+            this.txtSingkatan.Name = "txtSingkatan";
+            this.txtSingkatan.Size = new System.Drawing.Size(100, 22);
+            this.txtSingkatan.TabIndex = 10;
+            // 
+            // txtKaProdi
+            // 
+            this.txtKaProdi.Location = new System.Drawing.Point(239, 261);
+            this.txtKaProdi.Name = "txtKaProdi";
+            this.txtKaProdi.Size = new System.Drawing.Size(100, 22);
+            this.txtKaProdi.TabIndex = 11;
+            // 
+            // txtSekProdi
+            // 
+            this.txtSekProdi.Location = new System.Drawing.Point(239, 324);
+            this.txtSekProdi.Name = "txtSekProdi";
+            this.txtSekProdi.Size = new System.Drawing.Size(100, 22);
+            this.txtSekProdi.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 434);
+            this.Controls.Add(this.txtSekProdi);
+            this.Controls.Add(this.txtKaProdi);
+            this.Controls.Add(this.txtSingkatan);
+            this.Controls.Add(this.txtNamaProdi);
+            this.Controls.Add(this.txtIdProdi);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtIdProdi;
+        private System.Windows.Forms.TextBox txtNamaProdi;
+        private System.Windows.Forms.TextBox txtSingkatan;
+        private System.Windows.Forms.TextBox txtKaProdi;
+        private System.Windows.Forms.TextBox txtSekProdi;
+    }
+}
+
---
+++ P6_2_1204003/P6_2_1204003/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace P6_2_1204003
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgProdi = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProdi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgProdi
+            // 
+            this.dgProdi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProdi.Location = new System.Drawing.Point(39, 116);
+            this.dgProdi.Name = "dgProdi";
+            this.dgProdi.RowTemplate.Height = 24;
+            this.dgProdi.Size = new System.Drawing.Size(669, 232);
+            this.dgProdi.TabIndex = 0;
+            this.dgProdi.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(234, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FORM VIEW UPDATE PRODI";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(159, 386);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(82, 31);
+            this.btnRefresh.TabIndex = 2;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(419, 386);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(85, 31);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 462);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgProdi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProdi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgProdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
